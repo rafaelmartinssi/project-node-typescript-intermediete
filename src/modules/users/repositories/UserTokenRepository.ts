9b20dc3284cdd315f9a81c876ce031ffec100fd7
Rafael Martins Galdino <rafael_martins_182@hotmail.com>
2024-03-28 19:16:11 -0300

    Create migration and repository of UserToken

---
+++ src/modules/users/repositories/UserTokenRepository.ts
@@ -0,0 +1,26 @@
+import { Repository } from 'typeorm';
+import { dataSource } from '@shared/typeorm';
+import { IUserTokenRepository } from './IUserTokenRepository';
+import UserToken from '../entities/UserToken';
+
+class UserTokenRepository implements IUserTokenRepository {
+  private dataSource: Repository<UserToken>;
+
+  constructor() {
+    this.dataSource = dataSource.getRepository(UserToken);
+  }
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = this.dataSource.create({
+      user_id,
+    });
+
+    return this.dataSource.save(userToken);
+  }
+
+  public async findByToken(user_id: string): Promise<UserToken | null> {
+    return this.dataSource.findOneBy({ user_id });
+  }
+}
+
+export default UserTokenRepository;
--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,8 +1,10 @@
 import { DataSource } from 'typeorm';
 import { CreateProducts1710714805597 } from '@shared/typeorm/migrations/1710714805597-CreateProducts';
 import { CreateUsers1711316635135 } from '@shared/typeorm/migrations/1711316635135-CreateUsers';
+import { UserTokens1711661823496 } from './migrations/1711661823496-UserTokens';
 import Product from '@modules/products/entities/Product';
 import User from '@modules/users/entities/User';
+import UserToken from '@modules/users/entities/UserToken';
 
 const dataSource = new DataSource({
   type: 'postgres',
@@ -11,8 +13,12 @@ const dataSource = new DataSource({
   username: 'postgres',
   password: 'admin',
   database: 'apivendas',
-  entities: [Product, User],
-  migrations: [CreateProducts1710714805597, CreateUsers1711316635135],
+  entities: [Product, User, UserToken],
+  migrations: [
+    CreateProducts1710714805597,
+    CreateUsers1711316635135,
+    UserTokens1711661823496,
+  ],
 });
 
 export { dataSource };
